8f20527931bea992d5689c63854922c10efb9bdf
Micah Schuster <micahdschuster@gmail.com>
2017-06-03 18:09:59 -0400

    Finished Tutorial

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -14,27 +14,32 @@ public class GameManager : MonoBehaviour
     public TankManager[] m_Tanks;           
 
 
-    private int m_RoundNumber;              
-    private WaitForSeconds m_StartWait;     
+    private int m_RoundNumber;              //current round number
+    private WaitForSeconds m_StartWait;     //for coroutine waits
     private WaitForSeconds m_EndWait;       
-/*    private TankManager m_RoundWinner;
-    private TankManager m_GameWinner;       
-*/
+	private TankManager m_RoundWinner;      //winner of round (of type tankmanager, referes to specific tanks)
+	private TankManager m_GameWinner;       //winner of game (of type tankmanager)
+
 
     private void Start()
     {
+		//sets up waitforseconds
         m_StartWait = new WaitForSeconds(m_StartDelay);
         m_EndWait = new WaitForSeconds(m_EndDelay);
 
         SpawnAllTanks();
         SetCameraTargets();
 
+		//coroutine, stgarts the gameloop coroutine (each round round in gameloop)
+		//coroutine waits while round is being played, then round plays
         StartCoroutine(GameLoop());
     }
 
 
     private void SpawnAllTanks()
     {
+		//loops through tank manager, sets the instance in 
+		//that tank manager to the instantiated object, sets player number, runs setup (in tank manager)
         for (int i = 0; i < m_Tanks.Length; i++)
         {
             m_Tanks[i].m_Instance =
@@ -47,49 +52,101 @@ public class GameManager : MonoBehaviour
 
     private void SetCameraTargets()
     {
+		//get the number of tanks for array of transforms
         Transform[] targets = new Transform[m_Tanks.Length];
 
+		//loop through targets, set each target equal to the tank manager's instance transofm
         for (int i = 0; i < targets.Length; i++)
         {
             targets[i] = m_Tanks[i].m_Instance.transform;
         }
 
+		//sets the camera control's targets to the tank targets
         m_CameraControl.m_Targets = targets;
     }
 
-
+	//coroutines have return type IEnumerator
     private IEnumerator GameLoop()
     {
+		//yield causes stop execution of that function, waits for condition (or time)
+		//and comes back again and starts again when it left off
         yield return StartCoroutine(RoundStarting());
         yield return StartCoroutine(RoundPlaying());
         yield return StartCoroutine(RoundEnding());
 
-/*        if (m_GameWinner != null)
+        if (m_GameWinner != null)
         {
+			//reloading current level
             SceneManager.LoadScene(0);
         }
         else
         {
+			//no yeild return, function will finish, and there will be another game loop running
             StartCoroutine(GameLoop());
         }
-*/    }
+    }
 
-
+	//reset all tanks
+	//disable all tank controls
+	//set camera pos and size
+	//increment round number
+	//set message UI
     private IEnumerator RoundStarting()
     {
+		ResetAllTanks ();
+
+		DisableTankControl ();
+
+		m_CameraControl.SetStartPositionAndSize ();
+
+		m_RoundNumber++;
+
+		m_MessageText.text = "ROUND " + m_RoundNumber;
+
         yield return m_StartWait;
     }
 
-
+	//enable tank control
+	//Empty message UI, don't want to see "round n" all the time
+	//wait for one tank left
     private IEnumerator RoundPlaying()
     {
-        yield return null;
+		EnableTankControl ();
+
+		m_MessageText.text = string.Empty;
+
+		while (!OneTankLeft ()) {
+			yield return null; //with null, just means come back the next frame
+		}
+
+		//if execution makes it here, roundending is next
+
     }
 
-
+	//disable all tank contorls
+	//clear exsisted round winner
+	//check for game winner
+	//calculate message UI
     private IEnumerator RoundEnding()
     {
-        yield return m_EndWait;
+
+		DisableTankControl ();
+
+		m_RoundWinner = null; //tank manager, not an int or something
+
+		m_RoundWinner = GetRoundWinner ();  //finds tank that's active and returns it
+
+		if (m_RoundWinner != null) {
+			m_RoundWinner.m_Wins++;  //links to tank manager (where wins is held)
+		}
+
+		m_GameWinner = GetGameWinner ();
+
+		string message = EndMessage (); //get the message depending on above stuff
+
+		m_MessageText.text = message;
+
+        yield return m_EndWait; //sets the end wait
     }
 
 
@@ -106,12 +163,12 @@ public class GameManager : MonoBehaviour
         return numTanksLeft <= 1;
     }
 
-/*
+
     private TankManager GetRoundWinner()
     {
         for (int i = 0; i < m_Tanks.Length; i++)
         {
-            if (m_Tanks[i].m_Instance.activeSelf)
+            if (m_Tanks[i].m_Instance.activeSelf) //is this tank active?
                 return m_Tanks[i];
         }
 
@@ -123,7 +180,7 @@ public class GameManager : MonoBehaviour
     {
         for (int i = 0; i < m_Tanks.Length; i++)
         {
-            if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
+            if (m_Tanks[i].m_Wins == m_NumRoundsToWin) //is the number of wins that this tank has equal to the number of rounds needed to win the game
                 return m_Tanks[i];
         }
 
@@ -133,24 +190,29 @@ public class GameManager : MonoBehaviour
 
     private string EndMessage()
     {
-        string message = "DRAW!";
+        string message = "DRAW!"; //default condition
 
-        if (m_RoundWinner != null)
+        if (m_RoundWinner != null)  //if theres a round winner
             message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
 
         message += "\n\n\n\n";
 
-        for (int i = 0; i < m_Tanks.Length; i++)
+        for (int i = 0; i < m_Tanks.Length; i++)//these messages are for all tanks after every round
         {
-            message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
+			if (m_Tanks [i].m_Wins == 1) {
+				message += m_Tanks [i].m_ColoredPlayerText + ": " + m_Tanks [i].m_Wins + " WIN\n";
+
+			} else {
+				message += m_Tanks [i].m_ColoredPlayerText + ": " + m_Tanks [i].m_Wins + " WINS\n";
+			}
         }
 
-        if (m_GameWinner != null)
+        if (m_GameWinner != null) //is this tank the game winner? message is not added, this is all that is shown
             message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
 
         return message;
     }
-*/
+
 
     private void ResetAllTanks()
     {
--- Assets/Scripts/Managers/TankManager.cs
+++ Assets/Scripts/Managers/TankManager.cs
@@ -2,41 +2,44 @@
 using UnityEngine;
 
 [Serializable]
-public class TankManager
+public class TankManager   //not derived from monobehavior
 {
     public Color m_PlayerColor;            
     public Transform m_SpawnPoint;         
-    [HideInInspector] public int m_PlayerNumber;             
-    [HideInInspector] public string m_ColoredPlayerText;
-    [HideInInspector] public GameObject m_Instance;          
-    [HideInInspector] public int m_Wins;                     
+    [HideInInspector] public int m_PlayerNumber;            //shooting and moving scripts need the player number for input 
+    [HideInInspector] public string m_ColoredPlayerText;    //store colored text
+    [HideInInspector] public GameObject m_Instance;         //storing the instance of the tank
+    [HideInInspector] public int m_Wins;                    //number of wins that this tank has
 
 
-    private TankMovement m_Movement;       
+    private TankMovement m_Movement;       //references to tank moving and shooting
     private TankShooting m_Shooting;
-    private GameObject m_CanvasGameObject;
+    private GameObject m_CanvasGameObject; //referece to canvas, to turn UI on and off
 
 
     public void Setup()
     {
-        m_Movement = m_Instance.GetComponent<TankMovement>();
+        m_Movement = m_Instance.GetComponent<TankMovement>();  //getting tank components
         m_Shooting = m_Instance.GetComponent<TankShooting>();
-        m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
+        m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject; //finding component of canvas and getting game object
 
         m_Movement.m_PlayerNumber = m_PlayerNumber;
         m_Shooting.m_PlayerNumber = m_PlayerNumber;
 
+		//html-like rich text
         m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
 
+		//getting the meshrenderer of the tank (in child)
         MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
 
+		//changing all materials of all renderers to the player color
         for (int i = 0; i < renderers.Length; i++)
         {
             renderers[i].material.color = m_PlayerColor;
         }
     }
 
-
+	//disable moving, shooting and tank UI
     public void DisableControl()
     {
         m_Movement.enabled = false;
@@ -45,7 +48,7 @@ public class TankManager
         m_CanvasGameObject.SetActive(false);
     }
 
-
+	//enable moving, shooting, and tank UI
     public void EnableControl()
     {
         m_Movement.enabled = true;
@@ -54,12 +57,13 @@ public class TankManager
         m_CanvasGameObject.SetActive(true);
     }
 
-
+	//rest to spawn point
     public void Reset()
     {
         m_Instance.transform.position = m_SpawnPoint.position;
         m_Instance.transform.rotation = m_SpawnPoint.rotation;
 
+		//turn everything off, then back on again
         m_Instance.SetActive(false);
         m_Instance.SetActive(true);
     }
